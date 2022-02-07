780e38e8f35533c84ee4bb29ed2c1c901fd9cf4b
hueychopper <codelearn370@gmail.com>
2022-02-07 08:10:46 -0500

    numnonzerorows method

--- frq-solutions/ArrayResizer.java
+++ frq-solutions/ArrayResizer.java
@@ -1,3 +1,4 @@
+import java.util.*;
 public class ArrayResizer {
     public static void main(String[] args) {
         int[][] arr = {{2,1,0},{1,3,2},{0,0,0},{4,5,6}};
@@ -5,7 +6,7 @@ public class ArrayResizer {
         System.out.println(isNonZeroRow(arr,1));
         System.out.println(isNonZeroRow(arr,2));
         System.out.println(isNonZeroRow(arr,3));
-        resize(arr);
+        System.out.println(Arrays.deepToString(resize(arr)));
     }
     public static boolean isNonZeroRow(int[][] arr, int r) {
         for(int c = 0; c < arr[r].length; c++) {
@@ -15,8 +16,17 @@ public class ArrayResizer {
         }
         return true;
     }
+    public static int numNonZeroRows(int[][] arr) {
+        int count = 0;
+        for(int i = 0; i < arr.length; i++) {
+            if(isNonZeroRow(arr, i)) {
+                count++;
+            }
+        }
+        return count;
+    }
     public static int[][] resize(int[][] arr) {
-        int[][] smaller = new int[2][arr[0].length]; //2 is row number because the original problem had a method which return 2 as the answer. the method would've gone in the place of 2.
+        int[][] smaller = new int[numNonZeroRows(arr)][arr[0].length]; //2 is row number because the original problem had a method which return 2 as the answer. the method would've gone in the place of 2.
         int ix = 0;
         for(int i = 0; i < arr.length; i++) {
             if(isNonZeroRow(arr,i)) {
