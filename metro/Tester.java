53d8123abc0a873d7a75285d310be210c79257f1
hueychopper <75332544+hueychopper@users.noreply.github.com>
2022-02-28 08:33:37 -0500

    Update Tester.java

--- metro/Tester.java
+++ metro/Tester.java
@@ -4,7 +4,7 @@ class Tester {
     public static void main(String[] args) {
         //examples
         MetroCard mc = new MetroCard();
-        if(mc.getType() == "Special Access") {
+        if(mc.getType() == "Special Access") { //this can also be placed in the constructor
             mc.pay();
             // mc.cardTimer(); //this works in with real time. if this.timeLeft = 1, the program will
             //wait one minute to reallow metro card swipe;
